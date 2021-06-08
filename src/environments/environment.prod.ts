ef59d7f811687a7feb7bc1b3534c8f78d70e70ad
Satyanchal <62104921+Bluehatcoders@users.noreply.github.com>
2021-06-08 16:08:25 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: '<your-key>',
-    authDomain: '<your-project-authdomain>',
-    databaseURL: '<your-database-URL>',
-    projectId: '<your-project-id>',
-    storageBucket: '<your-storage-bucket>',
-    messagingSenderId: '<your-messaging-sender-id>',
-    appId: '<your-app-id>',
+   apiKey: "AIzaSyB_XI44VWLNUspyqMRjbAZAt9vTAb6LsIY",
+  authDomain: "learning-vhxdqt.firebaseapp.com",
+  databaseURL: "https://learning-vhxdqt.firebaseio.com",
+  projectId: "learning-vhxdqt",
+  storageBucket: "learning-vhxdqt.appspot.com",
+  messagingSenderId: "300528074788",
+  appId: "1:300528074788:web:da3b6c990169e2082258b1",
     measurementId: '<your-measurement-id>'
   },
   agoraAppId: '<your-agora-app-id>'
